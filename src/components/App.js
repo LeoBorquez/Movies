39d0f78c7996472563519b70eeea02645dcb371c
Leo Borquez <leoborquezg@protonmail.com>
2020-06-30 22:54:13 -0400

    🐛 FIX: search button

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import logo from '../logo.svg';
+import React, { useReducer, useEffect } from 'react';
 import '../App.css';
 import Header from './Header';
 import Movie from './Movie';
@@ -7,55 +6,95 @@ import Search from './Search';
 
 const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b" // api movies Duh!
 
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIE_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+    default:
+      return state;
+  }
+};
+
+
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]); // array of movies
-  const [errorMessage, setErrorMessage] = useState(null);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // calling api
   useEffect(() => {
     fetch(MOVIE_API_URL)
-    .then(response => response.json())
-    .then(jsonResponse => {
-      setMovies(jsonResponse.Search);
-      setLoading(false);
-    });
+      .then(response => response.json())
+      .then(jsonResponse => {
+        dispatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: jsonResponse.Search
+        });
+      });
   }, []); // first argument, the function that i want to use. second argument array that we pass in a value 
   // calling the useEffect once because the second argunment is a empty array
 
   const search = searchValue => {
-    setLoading(true);
-    setErrorMessage(null);
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
 
     fetch(`https://www.ombapi.com/?s=${searchValue}&apikey=4a3b711b`)
-    .then(response => response.json())
-    .then(jsonResponse => {
-      if(jsonResponse.Response === "True"){
-        setMovies(jsonResponse.Search);
-        setLoading(false);
-      } else {
-        setErrorMessage(jsonResponse.Error);
-        setLoading(false);
-      }
-    });
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.Search
+          });
+        } else {
+          dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.Error
+          });
+        }
+      });
   };
+
+  const { movies, errorMessage, loading } = state;
 
   return (
     <div className="App">
       {/* send the text property to the Header component */}
-      <Header text="MOVIES" /> 
-      <Search search="{search}" />
+      <Header text="MOVIES" />
+      <Search search={search} />
       <p className="App-intro"> Here a few of our favorite movies </p>
       <div className="movies">
         {loading && !errorMessage ? (
           <span>loading....</span>
         ) : errorMessage ? (
-        <div className="errorMessage">{errorMessage}</div>
+          <div className="errorMessage">{errorMessage}</div>
         ) : (
-          movies.map((movie, index) => (
-            <Movie key={`${index}-${movie.Title}`} movie={movie} />
-          ))
-        )}
+              movies.map((movie, index) => (
+                <Movie key={`${index}-${movie.Title}`} movie={movie} />
+              ))
+            )}
       </div>
     </div>
   );
